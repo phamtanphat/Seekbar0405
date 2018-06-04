233d1bbfcffb794e5e32da0ff212b1423fa6d18c
thuviengithub0208 <khoaphp@yahoo.com>
2018-06-04 19:13:10 +0700

    "Update"

--- app/src/main/java/com/ptp/phamtanphat/seekbar0405/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/seekbar0405/MainActivity.java
@@ -1,5 +1,7 @@
 package com.ptp.phamtanphat.seekbar0405;
 
+import android.os.Build;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,17 +9,31 @@ import android.widget.SeekBar;
 
 public class MainActivity extends    AppCompatActivity {
 
-    SeekBar sk;
+    SeekBar sk,sk2;
+    int value = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sk = findViewById(R.id.seekBar);
-        sk.setProgress(50);
+        sk2 = findViewById(R.id.seekBar2);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                sk.setProgress(sk.getProgress() + 10);
+                value = sk.getProgress();
+                handler.postDelayed(this,500);
+            }
+        },500);
+
         sk.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
+                if(seekBar.getProgress()  < value){
+                    seekBar.setProgress(value);
+                }
             }
 
             @Override
